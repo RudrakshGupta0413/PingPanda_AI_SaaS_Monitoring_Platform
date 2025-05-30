60e789d6a85cda80eb6e5fd890b2886407b41cbd
Rudraksh Gupta <rudrakshgupta40@gmail.com>
2025-05-30 15:20:46 +0530

    UX Fixes & Favicon Changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,9 +14,9 @@ const eb_garamond = EB_Garamond({
 })
 
 export const metadata: Metadata = {
-  title: "jStack App",
+  title: "PingPanda App",
   description: "Created using jStack",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+  icons: [{ rel: "icon", url: "/favicon-new.ico" }],
 }
 
 export default function RootLayout({
